1f3f5ab28e28c21dd90b35f0c138d23c56c4b8f6
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-28 14:37:36 +0530

    [Ritvik]Add: Welcome Message

---
+++ PersonDataManagement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PersonDataManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Person Data Management!");
+        }
+    }
+}
